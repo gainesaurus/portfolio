9fa34d54d8e65227b3afb0d4808b49c53e11fb36
gainey.michael <gainey.michael@gmail.com>
2023-09-05 13:26:45 -0700

    feat: contact section w netlify forms

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,12 +1,87 @@
+import { useState } from 'react';
 import styles from './Contact.module.css';
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  function encode(data:any) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
   return (
     <section id="contact" className={styles.container}>
       <h2 className={styles.heading}>Contact:</h2>
-      <input placeholder="Your Name"></input>
-      <input placeholder="Your E-Mail"></input>
-      <input placeholder="Your Message"></input>
+      <form
+        data-netlify="true"
+        name="contact"
+        onSubmit={handleSubmit}
+        className={styles.form}
+      >
+        <div className={styles.formField}>
+          <label htmlFor="name" className={styles.label}>
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your Name"
+            className={styles.nameInput}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label htmlFor="email" className={styles.label}>
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your E-Mail"
+            className={styles.emailInput}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.formField}>
+          <label
+            htmlFor="message"
+            className={styles.label}
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Your Message"
+            className={styles.msgInput}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className={styles.button}
+        >
+          Submit
+        </button>
+      </form>
     </section>
   );
 }
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return (
     <header className={styles.container}>
-      <h2 className={styles.navTitle}>gainesaur.io</h2>
+      <h2 className={styles.navTitle}>gainesaurus</h2>
       <a href="#projects" className={styles.links}>past work</a>
       <a href="#skills" className={styles.links}>skills</a>
       <a href="#contact" className={styles.links}>contact</a>
